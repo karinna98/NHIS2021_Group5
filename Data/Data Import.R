eb4e45075c66b3becd0cb2d6a925ac211e5dc2a2
Karinna Alatorre <karinna98@csu.fullerton.edu>
2026-04-13 21:00:37 -0700

    Add tidyverse and ggplot packages.

--- Data/Data Import.R
+++ Data/Data Import.R
@@ -2,6 +2,10 @@ library(readr)
 
 NHIS_Data_2021 <- read_csv("C:/Users/Karin/OneDrive/Spring 2026/PUBH 422/NHIS _Data_2021.csv")
 
+library(tidyverse)
+
+library(ggplot2)
+
 View(NHIS_Data_2021)
 
 str(NHIS_Data_2021)
