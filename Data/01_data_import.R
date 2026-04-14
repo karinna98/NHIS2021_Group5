bd3eb286f24a2d9e98d874b4f16a6c62d5f93f74
Karinna Alatorre <karinna98@csu.fullerton.edu>
2026-04-13 22:09:47 -0700

    Add comment for each line.

--- Data/01_data_import.R
+++ Data/01_data_import.R
@@ -1,15 +1,15 @@
-library(readr)
+library(readr) # opens 'readr'package
 
-NHIS_Data_2021 <- read_csv("C:/Users/Karin/OneDrive/Spring 2026/PUBH 422/NHIS _Data_2021.csv")
+NHIS_Data_2021 <- read_csv("C:/Users/Karin/OneDrive/Spring 2026/PUBH 422/NHIS _Data_2021.csv") # downloads data 
 
-library(tidyverse)
+library(tidyverse) # opens 'tidyverse'
 
-library(ggplot2)
+library(ggplot2) # opens 'ggplot2'
 
-View(NHIS_Data_2021)
+View(NHIS_Data_2021) # view data
 
-str(NHIS_Data_2021)
+str(NHIS_Data_2021) # examine structure of data
 
-summary (NHIS_Data_2021)
+summary (NHIS_Data_2021) # run statistical summary for all variables
 
-head (NHIS_Data_2021)
+head (NHIS_Data_2021) # print first 6 rows of data
