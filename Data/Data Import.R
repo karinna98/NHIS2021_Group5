d2a042a63bd0f67e35e13991f69ed3f0e93f8261
Karinna Alatorre <karinna98@csu.fullerton.edu>
2026-04-13 20:50:15 -0700

    str, summary, and head of data.

--- Data/Data Import.R
+++ Data/Data Import.R
@@ -1,3 +1,11 @@
 library(readr)
+
 NHIS_Data_2021 <- read_csv("C:/Users/Karin/OneDrive/Spring 2026/PUBH 422/NHIS _Data_2021.csv")
+
 View(NHIS_Data_2021)
+
+str(NHIS_Data_2021)
+
+summary (NHIS_Data_2021)
+
+head (NHIS_Data_2021)
