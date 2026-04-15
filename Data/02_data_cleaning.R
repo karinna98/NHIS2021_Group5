af68f7799ef6b4d4ad51715fedf95d300eb3227f
Karinna Alatorre <karinna98@csu.fullerton.edu>
2026-04-14 18:38:38 -0700

    Recode EDUCP_A

--- Data/02_data_cleaning.R
+++ Data/02_data_cleaning.R
@@ -20,4 +20,33 @@ dim (nhis_clean) # evaluates dimensions of 'nhis' clean data
 
 anyNA(nhis_clean) # examines 'nhis_clean' for any NA 
 
+summary (nhis_clean)
+
+nhis_clean <- nhis_clean %>% # selects 'nhis_clean' and create new version 
+  mutate( # modify variables in data set
+    EDUCP_A = case_when( # converting numerical variables to catergorical variables 
+      EDUCP_A %in% 0:3 ~ "Less than High School",
+      EDUCP_A == 4     ~ "High School Graduate",
+      EDUCP_A %in% 5:7 ~ "Some College Education",
+      EDUCP_A %in% 8:10 ~ "College Graduate or better",
+      TRUE ~ NA_character_  # assigns NA to any where the numbers do not apply 
+    ),
+    EDUCP_A = factor( # converts variable into a factor 
+      EDUCP_A,
+      levels = c(
+        "Less than High School",
+        "High School Graduate",
+        "Some College Education",
+        "College Graduate or better"
+      )
+    )
+  )
+
+summary (nhis_clean) # provides a statistical summary of 'hint_clean' data 
+
+view (nhis_clean) # view new clean data set 
+
+write_csv(nhis_clean, "data/nhis_clean.csv")
+
+
 
